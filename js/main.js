c476ecad7a8f22a09d09f9bff07da1841c22e232
Krendil766 <krendilek766@gmail.com>
2020-11-16 17:54:38 +0300

    Added edit post and push posts

--- js/main.js
+++ js/main.js
@@ -19,10 +19,8 @@ const editUserName = document.querySelector('.edit-username');
 const editPhotoUrl = document.querySelector('.edit-photo');
 const editBtn = document.querySelector('.edit-btn');
 const postsWarapper = document.querySelector('.posts');
-
-
-
-
+const buttonNewPost = document.querySelector('.button-new-post');
+const addPostElem = document.querySelector('.add-post');
 
 const listUsers = [
   {
@@ -43,35 +41,42 @@ const setUser = {
     const user = this.getUser(email);
     if(user && user.password === password){
       this.authorizedUser(user);
-      handler();
+      if(handler){
+        handler();
+      };
     }else{
       alert ('A user with this email is no autorized')
-    }
-    console.log(user);
+    };
   },
   logOut(handler){
     this.user=null;
-    handler();
+    if(handler){
+      handler();
+    };
   },
   signUp(email, password, handler){
     if(!this.getUser(email)){
+      if(handler){
+        handler();
+      };
       let user = {email, password, displayName: showName(email)};
       listUsers.push(user);
       this.authorizedUser(user);
-      handler();
     }else {
       alert ('A user with this email is registered')
     }
     console.log(listUsers);
   },
   editUser(userName, userPhoto='', handler){
+    if(handler){
+      handler();
+    };
     if(userName){
       this.user.displayName = userName;
     };
     if(userPhoto){
       this.user.photo = userPhoto;
     };
-    handler();
   },
   getUser(email){
     return listUsers.find(item => {
@@ -90,7 +95,7 @@ const setPosts = {
       title: "Heading",
       text: "Lалеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения,      ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит! ",
       tags: ['coll','new', 'hotter', 'mine', 'randomness'],
-      authort: 'krendilek766@gmail.com',
+      authort: {displayName: 'krendil', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIrcayxm7EZrNIkPciDCwVan0AnBMiWtxVwQ&usqp=CAU'},
       date: '11.11.2020, 10:02:32',
       like: 15,
       comments: 20,
@@ -98,13 +103,28 @@ const setPosts = {
       title: "Heading_two",
       text: "Lалеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения,      ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит! ",
       tags: ['coll','new', 'hotter', 'mine', 'randomness'],
-      authort: 'tatyanapopko@gmail.com',
+      authort: {displayName:'tatyana', photo:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTanNJ8wRbwuTECXAtI_USHqXEPa0OFxRMvbA&usqp=CAU'},
       date: '14.11.2020, 09:10:45',
       like: 45,
       comments: 10,
     }
-],
-
+  ],
+  addPost(title, text, tags, handler){
+    this.allPost.unshift({
+      title,
+      text,
+      tags: tags.split(',').map(item=>item.trim()),
+      authort:{displayName:setUser.user.displayName,
+          photo: setUser.user.photo,
+          },
+      date: new Date().toLocaleDateString(),
+      like:0,
+      comments:0,
+    });
+    if(handler){
+      handler();
+    }
+  },
 }
 
 const toggleAuthDom = ()=>{
@@ -114,10 +134,18 @@ const toggleAuthDom = ()=>{
     userElem.style.display = "";
     userNameElem.textContent = user.displayName;
     userAvatarElem.src = user.photo || userAvatarElem.src;
+    buttonNewPost.style.display = 'flex';
   }else{
     loginElem.style.display = '';
     userElem.style.display = "none";
+    addPostElem.classList.remove('visiable');
+    postsWarapper.classList.add('visiable');
   }
+}
+
+const showAddPost = () =>{
+  addPostElem.classList.add('visiable');
+  postsWarapper.classList.remove('visiable');
 }
 
 const validEmail=(str)=>{
@@ -140,14 +168,14 @@ const showName = (email)=>{
 
 const showAllPosts = () =>{
   let postsHTML = '';
-  setPosts.allPost.forEach(item => {
+  setPosts.allPost.forEach(({title, text, tags, like, comments, authort, date}) => {
     postsHTML +=`
         <section class="post">
         <div class="post-body">
-            <h2 class="post-title">${item.title}</h2>
-            <p class="post-text">${item.text}</p>
+            <h2 class="post-title">${title}</h2>
+            <p class="post-text">${text}</p>
             <div class="tags">
-                <a href="#" class="tag">${item.tags.map(item=>"#"+item).join(' ')}</a>
+                <a href="#" class="tag">${tags.map(item=>"#"+item).join(' ')}</a>
             </div>
         </div>
         <div class="post-footer">
@@ -156,13 +184,13 @@ const showAllPosts = () =>{
         <svg width="19" height="20" class="icon icon-like">
         <use xlink:href="img/icons.svg#like"></use>
         </svg>
-        <span class="likes-counter">${item.like}</span>
+        <span class="likes-counter">${like}</span>
         </button>
                 <button class="post-button comments">
         <svg width="21" height="21" class="icon icon-comment">
         <use xlink:href="img/icons.svg#comment"></use>
         </svg>
-        <span class="comments-counter">${item.comments}</span>
+        <span class="comments-counter">${comments}</span>
         </button>
                 <button class="post-button save">
         <svg width="19" height="19" class="icon icon-save">
@@ -177,10 +205,10 @@ const showAllPosts = () =>{
             </div>
             <div class="post-author">
                 <div class="author-about">
-                    <a href="#" class="author-username">${item.authort}</a>
-                    <span class="post-time">${item.date}</span>
+                    <a href="#" class="author-username">${authort.displayName}</a>
+                    <span class="post-time">${date}</span>
                 </div>
-                <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+                <a href="#" class="author-link"><img src="${authort.photo || "img/avatar.jpeg"}" alt="avatar" class="author-avatar"></a>
             </div>
         </div>
     </section>
@@ -235,6 +263,30 @@ const init = ()=>{
     e.preventDefault();
     setUser.editUser(editUserName.value, editPhotoUrl.value, toggleAuthDom);
     editContainer.classList.remove('visiable')
+  });
+
+  buttonNewPost.addEventListener('click', (e )=>{
+    e.preventDefault();
+    showAddPost();
+  });
+
+  addPostElem.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const formElements = addPostElem.elements;
+    const { title, text, tags } = formElements;
+    if(title.value.length<6){
+      alert('Short title');
+      return;
+    };
+    if(text.value.length<50){
+      alert('Short post');
+      return;
+    };
+
+    setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
+    addPostElem.classList.remove('visiable');
+    postsWarapper.classList.add('visiable');
+    addPostElem.reset();
   });
 
   showAllPosts();
